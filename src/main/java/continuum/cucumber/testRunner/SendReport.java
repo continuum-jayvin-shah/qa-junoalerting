5ede8f2d613fa826203855b2a4ad55ea1d03e21b
Bilal Khan <Bilal.Khan@continuum.net>
2019-01-17 19:29:32 +0530

    added Environment Info in Report

--- src/main/java/continuum/cucumber/testRunner/SendReport.java
+++ src/main/java/continuum/cucumber/testRunner/SendReport.java
@@ -152,7 +152,7 @@ public class SendReport {
 						+ Artifactory.formTheBuildVersionsForReporting(Artifactory.getLatestBuildNumberOfRespository());
 			}*/
 			
-			subject = "Automation Report for " + Utilities.getMavenProperties("ProjectName") + " on "
+			subject = Utilities.getMavenProperties("Environment") + "Automation Report for " + Utilities.getMavenProperties("ProjectName") + " on "
 					+ buildNo;
 
 			String message = "Automation Report for " + Utilities.getMavenProperties("ProjectName");
