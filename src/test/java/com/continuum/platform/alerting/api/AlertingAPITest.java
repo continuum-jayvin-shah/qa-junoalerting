6ea87bbf56d1a22d78c17d8bcba55b03fbfae9d1
rajat.sanu <rajat.sanu@continuum.com>
2020-01-28 14:49:19 +0530

    SLeep time of ITSM Simulator

--- src/test/java/com/continuum/platform/alerting/api/AlertingAPITest.java
+++ src/test/java/com/continuum/platform/alerting/api/AlertingAPITest.java
@@ -1141,7 +1141,7 @@ public class AlertingAPITest {
 
     public String getITSMSimulatorResponse() throws InterruptedException {
         String errMsg = "";
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         triggerITSMSimulatorAPI();
         try {
             if (alertingResponse.getStatusCode() == 200) {
